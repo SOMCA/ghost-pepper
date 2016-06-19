32e45c7306b9cb58f61c73b82952aab9266b96d1
k0pernicus <antonin.carette@gmail.com>
2016-06-19 16:16:43 -0400

    New implementation of the main program

--- src/ghost_pepper.py
+++ src/ghost_pepper.py
@@ -3,6 +3,8 @@ from lib.adb_utils import call_adb_command, get_output,\
 from lib.count import count_global_cs, rank
 from lib.monkey import MonkeyDetails
 from lib.progress_bar import ProgressBar
+
+from collections import defaultdict
 from time import sleep
 
 
@@ -17,6 +19,7 @@ def main():
     bar = ProgressBar(100, 100, "PROGRESSING...")
     bar.update(0)
     bar_step = 100/ITERATION
+    seed_to_details = defaultdict()
     for i in range(ITERATION):
         log_thread = call_adb_command("log", "-c")
         log_thread.wait()
@@ -25,10 +28,10 @@ def main():
                                                     throttle="300")
         monkey_thread.wait()
         monkey_output = get_output(monkey_thread)
-        print(MonkeyDetails(monkey_output))
         output = get_output(call_adb_command("log", "-d"))
         global_count = count_global_cs(output)
         values.append((seed, global_count))
+        seed_to_details[seed] = MonkeyDetails(monkey_output)
 
         stop_thread = call_shell_command("stop", APP)
         stop_thread.wait()
@@ -38,7 +41,12 @@ def main():
 
         sleep(5)
 
-    rank(values)
+    set_top_3 = rank(values)
+
+    for seed in set_top_3:
+        print("SEED %s" % seed)
+        print(seed_to_details[seed])
+        print("#"*40)
 
 
 if __name__ == '__main__':
