8cdfd4d16e6626c40a1e9803d3e44f6d92125db3
k0pernicus <antonin.carette@gmail.com>
2016-06-21 10:15:34 -0400

    Call call_command function instead of call_adb_command/call_shell_command

--- src/ghost_pepper.py
+++ src/ghost_pepper.py
@@ -1,5 +1,5 @@
-from lib.adb_utils import call_adb_command, get_output,\
-     launch_monkey_event, call_shell_command
+from lib.adb_utils import call_command, get_output,\
+     launch_monkey_event
 from lib.count import count_global_cs, rank
 from lib.monkey import MonkeyDetails
 from lib.progress_bar import ProgressBar
@@ -21,20 +21,20 @@ def main():
     bar_step = 100/ITERATION
     seed_to_details = defaultdict()
     for i in range(ITERATION):
-        log_thread = call_adb_command("log", "-c")
+        log_thread = call_command("log", "-c")
         log_thread.wait()
         (seed, monkey_thread) = launch_monkey_event(APP,
                                                     events="10000",
                                                     throttle="0")
         monkey_output, _ = monkey_thread.communicate()
-        output = get_output(call_adb_command("log", "-d"))
+        output = get_output(call_command("log", "-d"))
         global_count = count_global_cs(output)
         values.append((seed, global_count))
         seed_to_details[seed] = MonkeyDetails(monkey_output)
 
-        stop_thread = call_shell_command("stop", APP)
+        stop_thread = call_command("stop", APP)
         stop_thread.wait()
-        reset_thread = call_shell_command("reset", APP)
+        reset_thread = call_command("reset", APP)
         reset_thread.wait()
         bar.update((i + 1) * bar_step)
 
